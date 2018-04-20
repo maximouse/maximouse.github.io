6d9b39eab12414dad2094dcd25d12025dda8ecf1
maximouse <maximouse@users.noreply.github.com>
2018-04-20 20:07:35 +0300

    Add files via upload

--- telescope/main.js
+++ telescope/main.js
@@ -15,12 +15,17 @@ $(document).ready(function(){
     });
 
 
-
+    var counter=0;
     var native_width = 0;
     var native_height = 0;
     var win = false;
-    var dog1 = {x: 340, y: 290, checked: false}, dog2 = {x: 750, y: 380, checked: false}, dog3 = {x: 160, y: 765, checked: false}, dog4 = {x: 720, y: 790, checked: false}, dog5 = {x: 410, y: 1240, checked: false};
-
+    var dog1 = {x: 330, y: 330, checked: false}, dog2 = {x: 750, y: 450, checked: false}, dog3 = {x: 167, y: 900, checked: false}, dog4 = {x: 720, y: 880, checked: false}, dog5 = {x: 410, y: 1300, checked: false};
+    function count() {
+        ++counter;
+        $(".counter").text(counter);
+        $(".counter").fadeToggle(500);
+        $(".counter").fadeToggle(500);
+    }
     //$(".large").css("background","url('" + $(".small").attr("src") + "') no-repeat");
     $(".large").css("background","url('universe4k.jpg') no-repeat");
 
@@ -61,30 +66,35 @@ $(document).ready(function(){
                 console.log("Hit!");
                 dog1.checked = true;
                 console.log(dog1.checked);
+               count();
             }
             if((dog2.x+range>e.pageX && e.pageX>dog2.x-range) && (dog2.y+range>e.pageY && e.pageY>dog2.y-range) && dog2.checked == false){
 
                 console.log("Hit! 2");
                 dog2.checked = true;
                 console.log(dog2.checked);
+                count();
             }
             if((dog3.x+range>e.pageX && e.pageX>dog3.x-range) && (dog3.y+range>e.pageY && e.pageY>dog3.y-range) && dog3.checked == false){
 
                 console.log("Hit 3!");
                 dog3.checked = true;
                 console.log(dog3.checked);
+                count();
             }
             if((dog4.x+range>e.pageX && e.pageX>dog4.x-range) && (dog4.y+range>e.pageY && e.pageY>dog4.y-range) && dog4.checked == false){
 
                 console.log("Hit 4!");
                 dog4.checked = true;
                 console.log(dog4.checked);
+                count();
             }
             if((dog5.x+range>e.pageX && e.pageX>dog5.x-range) && (dog5.y+range>e.pageY && e.pageY>dog5.y-range) && dog5.checked == false){
 
                 console.log("Hit 5!");
                 dog5.checked = true;
                 console.log(dog5.checked);
+                count();
             }
 
             if(dog1.checked == true && dog2.checked == true && dog3.checked == true && dog4.checked == true && dog5.checked == true && win==false){
@@ -141,3 +151,4 @@ $(window).load(function () {
 
 
 });
+
